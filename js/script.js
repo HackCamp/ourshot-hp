841c6708ebf561c7ac6a210bf81d55753d5be0f2
sel <ikd618@gmail.com>
2016-07-04 12:33:34 +0900

    change arrow icons

--- js/script.js
+++ js/script.js
@@ -75,11 +75,11 @@ $(function(){
     if($answer.hasClass('open')) {
       $answer.removeClass('open');
       $answer.slideUp();
-      $(this).find('span').text('+');
+      $(this).find('span').text('▼');
     } else {
       $answer.addClass('open');
       $answer.slideDown();
-      $(this).find('span').text('-');
+      $(this).find('span').text('▲');
     }
   });
 
